3e4045cec7c68644546457dc4c4f2f414b89a7d8
1371101711970002 <farhanrafid97@gmail.com>
2023-01-12 15:19:23 +0700

    package isvalid initial commmit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/FarhanRafid97/go-isValid-mod
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,41 @@
+package goisvalidmod
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type Sample struct{
+	Name string `required:"true" max:"10"`
+	Age int `required:"tidak" max:"2"`
+	Address string `required:"true" max:"32"`
+}
+
+type IsValidCase struct{
+	Name string
+	IsValid bool
+}
+
+func IsValid(data interface{})[]IsValidCase {
+	fields := []IsValidCase{}
+	t := reflect.TypeOf(data)
+	for i:=0;i < t.NumField();i++{
+		field := t.Field(i)
+		fmt.Println(field)
+		if field.Tag.Get("required") == "true" {
+			fields = append(fields, IsValidCase{
+				Name:field.Name,
+				IsValid: reflect.ValueOf(data).Field(i).Interface() != "",
+			})
+		
+		}else{
+
+			fields = append(fields,IsValidCase{
+				Name:field.Name,
+				IsValid: true,
+			})
+		}
+	}
+	
+	return fields
+}
